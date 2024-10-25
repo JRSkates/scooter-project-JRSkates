bbce5e413edaf644bddfb194eabe505b310e4654
JRSkates <skatesproduction@gmail.com>
2024-10-25 13:38:22 +0100

    ScooterApp half test driven

--- __tests__/ScooterApp.test.js
+++ __tests__/ScooterApp.test.js
@@ -1,45 +1,62 @@
 const { describe, expect, it } = require("@jest/globals");
 const ScooterApp = require("../classes/ScooterApp.js");
+const User = require("../classes/User.js");
+
+describe("ScooterApp setup", () => {
+  const app = new ScooterApp();
+  it("should initialize a ScooterApp", () => {
+    expect(app.stations).toEqual({
+      "Station A": [],
+      "Station B": [],
+      "Station C": []
+    });
+    expect(app.registeredUsers).toEqual({});
+  });
+})
 
 describe("ScooterApp.registerUser(username, password, age)", () => {
-  it.skip("registers a new user if old enough", () => {
-    // Arrange
-    // Act
-    // Assert
+  const app = new ScooterApp();
+  it("registers a new user if old enough", () => {
+    app.registerUsers("JRSkates", "password", 28)
+    expect(app.registeredUsers["JRSkates"]).toBeTruthy();
   });
 
-  it.skip("throws an error if too young or already registered", () => {
-    // Arrange
-    // Act
-    // Assert
+  it("throws an error if too young or already registered", () => {
+    expect(() => app.registerUsers("TestUser1", "newpassword", 10)).toThrow("too young to register")
+
+    app.registerUsers("JackUser", "password", 28)
+    expect(() => app.registerUsers("JackUser", "password2", 34)).toThrow("already registered");
   });
 });
 
 describe("ScooterApp.loginUser(username, password)", () => {
-  it.skip("logs in a registered user", () => {
-    // Arrange
-    // Act
-    // Assert
+  const app = new ScooterApp();
+  it("logs in a registered user", () => {
+    app.registerUsers("JRSkates", "password", 28)
+    console.log(app.registeredUsers);
+    app.loginUser("JRSkates", "password");
+    expect(app.registeredUsers["JRSkates"].isLoggedIn).toBe(true);
   });
 
-  it.skip("throws an error if user not found or password incorrect", () => {
-    // Arrange
-    // Act
-    // Assert
+  it("throws an error if user not found or password incorrect", () => {
+    expect(() => app.loginUser("TestUser1", "newpassword")).toThrow("Username or password is incorrect")
+
+    app.registerUsers("JackSkates", "password1", 29)
+    expect(() => app.loginUser("JackSkates", "passwordwrong")).toThrow("Username or password is incorrect")
   });
 });
 
 describe("ScooterApp.logoutUser(username)", () => {
-  it.skip("logs out a registered user", () => {
-    // Arrange
-    // Act
-    // Assert
+  const app = new ScooterApp();
+  it("logs out a registered user", () => {
+    app.registerUsers("JRSkates", "password", 28)
+    app.loginUser("JRSkates", "password");
+    app.logoutUser("JRSkates");
+    expect(app.registeredUsers["JRSkates"].isLoggedIn).toBe(false);
   });
 
-  it.skip("throws an error if user not found", () => {
-    // Arrange
-    // Act
-    // Assert
+  it("throws an error if user not found", () => {
+    expect(() => app.logoutUser("TestUser")).toThrow("no such user is logged in");
   });
 });
 
--- classes/ScooterApp.js
+++ classes/ScooterApp.js
@@ -2,7 +2,46 @@ const Scooter = require("./Scooter.js");
 const User = require("./User.js");
 
 class ScooterApp {
-  // ScooterApp code here
+  constructor() {
+    this.stations = {
+        "Station A": [],
+        "Station B": [],
+        "Station C": []
+    };
+    this.registeredUsers = {};
+  }
+
+  registerUsers(username, password, age) {
+    if (age < 18) {
+      throw new Error("too young to register");
+    } else if (this.registeredUsers[username]) {
+      throw new Error("already registered");
+    } else {
+      this.registeredUsers[username] = new User(username, password, age);
+    }
+  }
+
+  loginUser(username, password) {
+    const user = this.registeredUsers[username];
+    if (!user) {
+      throw new Error("Username or password is incorrect");
+    } else if (user.password !== password) {
+      throw new Error("Username or password is incorrect");
+    } else {
+      user.isLoggedIn = true;
+    }
+  }
+
+  logoutUser(username) {
+    const user = this.registeredUsers[username];
+    if (!user) {
+      throw new Error("no such user is logged in");
+    } else {
+      user.isLoggedIn = false;
+      console.log("user is logged out");
+    }
+  }
+
 }
 
 module.exports = ScooterApp;
